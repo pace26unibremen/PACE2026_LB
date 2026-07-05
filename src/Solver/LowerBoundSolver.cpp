fb4e896a6b046253e089527857ac5a6cf7de117e
Jurin Hoffmann <jurin@uni-bremen.de>
2026-07-05 12:23:42 +0200

    added while loop...

--- src/Solver/LowerBoundSolver.cpp
+++ src/Solver/LowerBoundSolver.cpp
@@ -26,11 +26,29 @@ bool LowerBoundSolver::solve()
         maxsatSolver->solve();
         int maxsatLB = static_cast<int> (std::floor((context->a * maxsatSolver->getCurrentLowerBound()) + context->b));
 
+        // Hier fehlt mir noch der Eintrag im Context...
+        // if (maxsatLB > context->lb) 
+        // {
+        //     context->lb = maxsatLB;
+        // }
+        // else
+        // {
+        //     double frac = (i >= 3) ? 1.0 : (2.0 * i - 1.0) / (2.0 * i);
+        //     if (maxsatLB < frac * context->lb)
+        //     {
+        //         break;
+        //     }
+        // }
 
+        if (++i > 3) break;
     }
-    // Funktionscalls für maxsatSolver:
-    // maxsatSolver->solve();
-    // maxsatSolver->setTimeOut(double second);
-    // maxsatSolver->getCurrentLowerBound();
+
+    if (!solved)
+    {
+        // branchingSolver->SetTimeout(600); oder so ähnlich...
+        solved = branchingSolver->solve();
+    }
+    return solved;
 }
+
 } // namespace solver
